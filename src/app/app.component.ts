29565325b4198205179695090e848aa0cd2c2118
admin <admin@d98.w11k>
2020-09-01 16:30:17 +0200

    interface exercice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+
+interface Training {
+  id: number;
+  name: string;
+  description: string;
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'client';
+  public tr1: Training;
+
+  constructor() {
+    this.tr1 = {
+      id = 425;
+      name = "tennis" ;
+      description = "nadal is the best"
+    }
+  }
+
 }
