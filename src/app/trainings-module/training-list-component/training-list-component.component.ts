1502f1c5e7732f36eb5cc37fa29600a2954a3b9e
admin <admin@d98.w11k>
2020-09-01 17:35:10 +0200

    input exercice

--- src/app/trainings-module/training-list-component/training-list-component.component.ts
+++ src/app/trainings-module/training-list-component/training-list-component.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Training} from '../../app.component';
 
 @Component({
   selector: 'app-training-list-component',
   templateUrl: './training-list-component.component.html',
   styleUrls: ['./training-list-component.component.scss']
 })
-export class TrainingListComponentComponent implements OnInit {
 
-  constructor() { }
+
+export class TrainingListComponentComponent implements OnInit {
+  @Input() manyTrainings : Training[];
+
 
   ngOnInit(): void {
   }
