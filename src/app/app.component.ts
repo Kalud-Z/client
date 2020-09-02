e5e492787979c37296ed0840b7a3630efabf0db2
kalud <khaled.fhm@live.fr>
2020-09-02 15:46:54 +0200

    using service now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import {Training} from './trainings-module/training.model';
 import {TrainingSelectedEvent} from './trainings-module/trainingSelectedEvent';
+import {TrainingService} from './trainings-module/training.service';
 
 
 
@@ -10,16 +12,15 @@ import {TrainingSelectedEvent} from './trainings-module/trainingSelectedEvent';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
-  title : string;
+export class AppComponent implements OnInit {
   selectedTraining: Training;
+  allTrainings: Training[];
 
-  allTrainings: Training[] = [
-    { id : 1 , name : 'Javascript' , discontinued : false ,  description : 'Mother of the Web' , imageUrl : "assets/images/angular2-shield.svg" },
-    { id : 2 , name : 'Pascal'  , discontinued : true , description : 'First language to meet' , imageUrl : "assets/images/angularjs-shield.svg"},
-    { id : 3 , name : 'C++' , discontinued : false , description : 'Dont like it :|' , imageUrl : "assets/images/typescript-logo.svg"},
-  ]
+  constructor(private trainingService: TrainingService) {}
 
+  ngOnInit() {
+    this.allTrainings = this.trainingService.getAll();
+  }
 
   getLastSelectedTraining(event: TrainingSelectedEvent) {
     this.selectedTraining = event.selectedTraining;
--- src/app/trainings-module/training.mock.ts
+++ src/app/trainings-module/training.mock.ts
@@ -1,10 +1,10 @@
 import {Training} from './training.model';
 
-
-export const TRAININGS: Training[] =
+export const TRAININGS: Training[] = [
   { id : 1 , name : 'Javascript' , discontinued : false ,  description : 'Mother of the Web' , imageUrl : "assets/images/angular2-shield.svg" },
   { id : 2 , name : 'Pascal'  , discontinued : true , description : 'First language to meet' , imageUrl : "assets/images/angularjs-shield.svg"},
   { id : 3 , name : 'C++' , discontinued : false , description : 'Dont like it :|' , imageUrl : "assets/images/typescript-logo.svg"},
 ]
+
 
 
