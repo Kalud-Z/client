75461ffe8b45509ddbf4961fbac3c758d85242a5
kalud <khaled.fhm@live.fr>
2020-09-02 15:24:42 +0200

    two way binding

--- src/app/trainings-module/training.model.ts
+++ src/app/trainings-module/training.model.ts
@@ -4,5 +4,5 @@ export interface Training {
   name: string;
   discontinued: boolean;
   description?: string;
-  imageUrl?: string
+  imageUrl?: string;
 }
--- src/app/trainings-module/trainings-module.module.ts
+++ src/app/trainings-module/trainings-module.module.ts
@@ -2,12 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrainingListComponentComponent } from './training-list-component/training-list-component.component';
 import { TrainingsDetailsComponent } from './trainings-details/trainings-details.component';
+import {FormsModule} from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [TrainingListComponentComponent, TrainingsDetailsComponent],
-  imports: [CommonModule],
-  exports : [ TrainingListComponentComponent , TrainingsDetailsComponent ]
+  declarations: [
+    TrainingListComponentComponent,
+    TrainingsDetailsComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  exports : [
+    TrainingListComponentComponent,
+    TrainingsDetailsComponent ]
 })
 export class TrainingsModuleModule { }
